cb5ec35acad342aa6f78703a982017edbb2fdc14
RajeevRanjan5642 <rajeevranjan31012003@gmail.com>
2023-06-05 21:46:00 +0530

    added code for speech recognition

--- speech_reg.py
+++ speech_reg.py
@@ -1,3 +1,26 @@
-# Author   : Rajeev Ranjan
-# Date     : June 5 2023 8:01 PM
-# Objective: To create a speech to text converter system
+ """
+
+ Author   : Rajeev Ranjan
+ Date     : June 5 2023 8:01 PM
+ Objective: To create a speech to text converter system
+ 
+ """
+
+import speech_recognition as sr
+
+#create a variable for storing/refering the sample audio file
+audio_file=("sample.wav")
+
+r=sr.Recognizer() #initialise the recogniser
+
+with sr.WavFile(audio_file) as source: #use audio_file as source
+    audio=r.record(source) #extracts the audio file data
+    
+try:
+    print(r.recognize_google(audio)) #recognize the audio using google speech recognition
+    #exception handling
+except sr.UnknownValueError :
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError :
+    print("couldn't get the results from Google Speech Recognition")
+
